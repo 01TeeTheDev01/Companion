af0c69a238c206aab0c4f08cce37643eb77ec1f9
Teethedev@outlook.com <Teethedev@outlook.com>
2022-06-14 18:56:41 +0200

    Removed notifications page

--- TaxiAccountantV2/TaxiAccountantV2/Services/Emergency Services/EmergencyService.cs
+++ TaxiAccountantV2/TaxiAccountantV2/Services/Emergency Services/EmergencyService.cs
@@ -17,7 +17,7 @@ namespace TaxiAccountantV2.Services.Emergency_Services
                 new EmergencyModel{ ServiceName = "NETCARE 911", ServiceNumber = "082 911" },
                 new EmergencyModel{ ServiceName = "AMBULANCE", ServiceNumber = "10177" },
                 new EmergencyModel{ ServiceName = "FIRE DEPARTMENT", ServiceNumber = "10177" },
-                new EmergencyModel{ ServiceName = "ER24 ", ServiceNumber = "084 124" },
+                new EmergencyModel{ ServiceName = "ER24", ServiceNumber = "084 124" },
                 new EmergencyModel{ ServiceName = "MOUNTAIN RESCUE – KZN", ServiceNumber = "031 307 7744" },
                 new EmergencyModel{ ServiceName = "MOUNTAIN RESCUE – WESTERN CAPE", ServiceNumber = "021 948 9900" },
                 new EmergencyModel{ ServiceName = "MOUNTAIN RESCUE – GAUTENG", ServiceNumber = "074 125 1385 / 074 163 3952" },
@@ -32,7 +32,8 @@ namespace TaxiAccountantV2.Services.Emergency_Services
                 new EmergencyModel{ ServiceName = "HEALTH & ACCIDENTS - LIMPOPO", ServiceNumber = "0800 919 191" },
                 new EmergencyModel{ ServiceName = "HEALTH & ACCIDENTS - MPUMALANGA", ServiceNumber = "0800 204 098" },
                 new EmergencyModel{ ServiceName = "HEALTH & ACCIDENTS - NORTHERN CAPE", ServiceNumber = "018 387 5778" },
-                new EmergencyModel{ ServiceName = "HEALTH & ACCIDENTS - WESTERN CAPE", ServiceNumber = "021 483 5624" }
+                new EmergencyModel{ ServiceName = "HEALTH & ACCIDENTS - WESTERN CAPE", ServiceNumber = "021 483 5624" },
+                new EmergencyModel{ ServiceName = "POLICE", ServiceNumber = "10111"}
             };
         }
         public Task<IEnumerable<EmergencyModel>> GetEmergencyServices()
